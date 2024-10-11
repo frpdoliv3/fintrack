85545b82c4fa27c49df6ce3aa15da08171b5e26e
Francisco Oliveira <fr.pdoliv@gmail.com>
2024-10-11 11:15:38 +0100

    feat: changed routing to kebab case

---
+++ FinTrack/PascalToKebabParameterTransformer.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FinTrack;
+
+public sealed class PascalToKebabParameterTransformer : IOutboundParameterTransformer
+{
+    public string? TransformOutbound(object? value)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+        var str = value.ToString();
+        return string.IsNullOrEmpty(str) ? 
+            null : 
+            Regex.Replace(str, "([a-z])([A-Z])", "$1-$2").ToLower();
+    }
+}
--- FinTrack/Program.cs
+++ FinTrack/Program.cs
@@ -1,8 +1,20 @@
+using System.Text.Json;
+using FinTrack;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opts =>
+{
+    opts.ModelMetadataDetailsProviders.Add(new SystemTextJsonValidationMetadataProvider());
+    opts.Conventions.Add(new RouteTokenTransformerConvention(new PascalToKebabParameterTransformer()));
+}).AddJsonOptions(opts =>
+{
+    opts.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
