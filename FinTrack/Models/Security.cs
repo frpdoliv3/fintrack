8571927362e3b0471f6dca2597017e1b87dff753
Francisco Oliveira <fr.pdoliv@gmail.com>
2024-10-13 17:02:03 +0100

    feat: isin validation

---
+++ FinTrack/Models/Security.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FinTrack.Validation;
+
+namespace FinTrack.Models;
+
+public class Security
+{
+    [Required(AllowEmptyStrings = false)]
+    public string Name { get; set; } = String.Empty;
+    
+    [Required(AllowEmptyStrings = false)]
+    [IsIsin]
+    public string Isin { get; set; } = String.Empty;
+
+    [Required(AllowEmptyStrings = false)]
+    public string NativeCurrency { get; set; } = String.Empty;
+    
+    [Required(AllowEmptyStrings = false)]
+    public string CounterpartyCountry { get; set; } = String.Empty;
+}
---
+++ FinTrack/Validation/IsIsin.cs
@@ -0,0 +1,15 @@
+﻿using CheckDigits.Net;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinTrack.Validation;
+
+public sealed class IsIsin: ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is not string stringValue) { return false; }
+
+        var isinAlgorithm = Algorithms.Isin;
+        return isinAlgorithm.Validate(stringValue);
+    }
+}
